e809f772a68019bd76f41280bad5c57e528381bb
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2020-09-26 18:19:50 +0700

    customized elements @49.00

--- src/components/Navbar/Navbar.elements.js
+++ src/components/Navbar/Navbar.elements.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { FaCompactDisc } from "react-icons/fa";
 import { Container } from "../../globalStyles";
 
 export const Nav = styled.nav`
-	background: #79a29e;
+	background: #0c0f0a;
 	height: 80px;
 	display: flex;
 	justify-content: center;
@@ -19,4 +21,100 @@ export const NavbarContainer = styled(Container)`
 	height: 80px;
 
 	${Container};
+`;
+
+export const NavLogo = styled(Link)`
+	color: #f3efe4;
+	justify-self: flex-start;
+	cursor: pointer;
+	text-decoration: none;
+	font-size: 2rem;
+	display: flex;
+	align-items: center;
+`;
+
+export const NavIcon = styled(FaCompactDisc)`
+	margin-right: 0.5rem;
+`;
+
+export const HamburgerMenu = styled.div`
+	display: none;
+
+	@media screen and (max-width: 960px) {
+		display: block;
+		position: absolute;
+		top: 0;
+		right: 0;
+		transform: translate(-100%, 60%);
+		font-size: 1.8rem;
+		cursor: pointer;
+	}
+`;
+
+export const NavMenu = styled.ul`
+	display: flex;
+	align-items: center;
+	list-style: none;
+	text-align: center;
+
+	@media screen and (max-width: 960px) {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		height: 90vh;
+		position: absolute;
+		top: 80px;
+		left: ${({ click }) => (click ? 0 : "-100%")};
+		opacity: 1;
+		transition: all 0.5s ease;
+		background: #3fbc97;
+	}
+`;
+
+export const NavItem = styled.li`
+	height: 80px;
+	border-bottom: 2px solid transparent;
+
+	&:hover {
+		border-bottom: 5px solid #dd403a;
+	}
+
+	@media screen and (max-width: 960px) {
+		width: 100%;
+
+		&:nth-child(1) {
+			background: #00a676;
+		}
+		&:nth-child(2) {
+			background: #17ae82;
+		}
+		&:nth-child(3) {
+			background: #2cb58d;
+		}
+
+		&:hover {
+			border: none;
+		}
+	}
+`;
+
+export const NavLinks = styled(Link)`
+	display: flex;
+	align-items: center;
+	text-decoration: none;
+	padding: 0.5rem 1.2rem;
+	height: 100%;
+
+	@media screen and (max-width: 960px) {
+		color: #0c0f0a;
+		text-align: center;
+		padding: 2rem;
+		width: 100%;
+		display: table;
+
+		&:hover {
+			color: #f3efe4;
+			transition: all 0.3s ease;
+		}
+	}
 `;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,53 @@
-import React from "react";
-import { Nav, NavbarContainer } from "./Navbar.elements";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import {
+	Nav,
+	NavbarContainer,
+	NavLogo,
+	NavIcon,
+	HamburgerMenu,
+	NavMenu,
+	NavItem,
+	NavLinks,
+} from "./Navbar.elements";
 
 const Navbar = () => {
+	const [click, setClick] = useState(false);
+
+	// handle navbar click
+	const handleClick = () => setClick(!click);
+
 	return (
 		<>
-			<Nav>
-				<NavbarContainer>
-					<NavLogo to="/">
-						<NavIcon />
-						Vinylly
-					</NavLogo>
-				</NavbarContainer>
-			</Nav>
+			<IconContext.Provider value={{ color: "#F3EFE4" }}>
+				<Nav>
+					<NavbarContainer>
+						<NavLogo to="/">
+							<NavIcon />
+							Vinylly
+						</NavLogo>
+
+						<HamburgerMenu onClick={handleClick}>
+							{click ? <FaTimes /> : <FaBars />}
+						</HamburgerMenu>
+						{/* Menu list start here */}
+						<NavMenu onClick={handleClick} click={click}>
+							<NavItem>
+								<NavLinks to="/">home</NavLinks>
+							</NavItem>
+
+							<NavItem>
+								<NavLinks to="/products">vinyl</NavLinks>
+							</NavItem>
+
+							<NavItem>
+								<NavLinks to="/about">about</NavLinks>
+							</NavItem>
+						</NavMenu>
+					</NavbarContainer>
+				</Nav>
+			</IconContext.Provider>
 		</>
 	);
 };
